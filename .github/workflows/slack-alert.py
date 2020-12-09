3333f267a42564a3674b55bb1ec0596024f7e7ec
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2020-12-09 13:45:07 -0500

    Update slack-alert.py

--- .github/workflows/slack-alert.py
+++ .github/workflows/slack-alert.py
@@ -60,17 +60,18 @@ if __name__ == "__main__":
     parser.add_argument('driverlocation', type=str)
     parser.add_argument('devdate', type=str)
     parser.add_argument('status', type=str)
-    parser.add_argument('url', type=str)
+    parser.add_argument('ownerRepo', type=str)
+    parser.add_argument('runID', type=str)
     parser.add_argument('slackhook', type=str)
     args = parser.parse_args()
     
-    message["attachments"][0]["blocks"][1]["fields"][0]["text"] = f"*Status:*\n {args.status.upper()}"
+    message["attachments"][0]["blocks"][1]["fields"][0]["text"] = f"*Status:*\n {args.status}"
     message["attachments"][0]["blocks"][1]["fields"][1]["text"] = f"*Dev Date:*\n {args.devdate}"
     message["attachments"][0]["blocks"][1]["fields"][2]["text"] = f"*Build Level:*\n {args.buildlevel}"
     message["attachments"][0]["blocks"][2]["text"]["text"] = f"*Driver Location:* {args.driverlocation}"
-    message["attachments"][0]["blocks"][3]["text"]["text"] = f"*<{args.url}| Builds>*"
+    workflow_url = f"https://github.com/{args.ownerRepo}/actions/runs/{args.runID}"
+    message["attachments"][0]["blocks"][3]["text"]["text"] = f"*<{workflow_url}| Builds>*"
     
-
     status = args.status
     if status == 'failure':
         message["attachments"][0]["color"] = "#d73a49"
@@ -79,6 +80,7 @@ if __name__ == "__main__":
     else:
 	message["attachments"][0]["color"] = "#f6f8fa"
 	
+    
     requests.post(args.slackhook, headers=headers, data=json.dumps(message))
     
 
